b46fbf9cff5ec1c69d164d7ab4e71276752b63f6
cignoir <cignoir@gmail.com>
2017-05-19 01:26:00 +0900

    Update select_half.py

--- select_half/select_half.py
+++ select_half/select_half.py
@@ -5,12 +5,13 @@
 
 import lx
 
-layer_index = lx.eval("query layerservice layer.index ? main")
+fg = lx.eval("query layerservice layers ? fg")
 verts = lx.eval("query layerservice verts ? all")
 
 if verts is None:
     lx.out("No geometry")
 else:
-    for vert_index, vert in enumerate(verts):
-        if vert.pos[1] < 0:
-            lx.eval("select.element layer:\"%i\" type:vertex mode:add index:%i" % (layer_index, vert_index))
+    for v in verts:
+        pos = lx.eval("query layerservice vert.pos ? {0}".format(v))
+        if pos[0] <= -0.00000001:
+            lx.eval("select.element {0} vertex add index:{1}".format(fg,v))
